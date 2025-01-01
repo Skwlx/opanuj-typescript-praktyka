0f7c34a7cf78b8da708c9e30576adec01797e6f2
Paweł Sławuta <p.slawuta@limur.pl>
2025-01-01 16:19:55 +0100

    fix: 442

--- tasks/core-pro/442-infer-flags/task.ts
+++ tasks/core-pro/442-infer-flags/task.ts
@@ -1,11 +1,17 @@
 import { type FeatureFlags } from './legacy-flags.ts';
 
-type FlagsV2<T> = {};
+type FlagsV2<T> = {
+  [K in keyof T as K extends `${infer R}V2Enabled` ? `${R}Enabled` : never]: boolean;
+};
 
 export type ModernFeatureFlags = FlagsV2<FeatureFlags>;
 
 export function getFeatureFlagsV2(flags: FeatureFlags): ModernFeatureFlags {
-  const flagsV2: ModernFeatureFlags = {};
+  const flagsV2: ModernFeatureFlags = {
+    isCartEnabled: flags.isCartV2Enabled,
+    isSearchEnabled: flags.isSearchV2Enabled,
+    isContactFormEnabled: flags.isContactFormV2Enabled,
+  };
 
   return flagsV2;
 }
